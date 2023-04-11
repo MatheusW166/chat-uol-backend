f6c4dc06f51ae9e0afd6d79799e3800141e630f2
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-11 18:26:20 -0300

    chore: dotenv

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+import { config } from "dotenv";
+config();
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT ?? 5000;
+app.listen(PORT, () => console.log(`Running at PORT ${PORT}`));
