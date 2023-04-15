ef5840a460dddfd6dac576b80f559e6e45c446ea
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-04-15 16:32:09 -0300

    fix: Forcing string before html strip

--- src/adapters/validation/validation.adapter.js
+++ src/adapters/validation/validation.adapter.js
@@ -6,44 +6,43 @@ class ValidationAdapter {
     return errors?.details.map((err) => err.message) ?? [];
   };
   validateObjectId = (id) => {
-    const strippedId = stripHtml(id ?? "").result;
     const stringSchema = Joi.string()
       .trim()
       .pattern(/^([0-9a-f]{12}|[0-9a-f]{24})$/i);
-    const { error, value } = stringSchema.validate(strippedId);
+    const { error, value } = stringSchema.validate(id);
     const errorMessagesArray = this.convertErrors(error);
     return { error: errorMessagesArray, value };
   };
   validateString = (string) => {
-    const strippedString = stripHtml(string ?? "").result;
     const stringSchema = Joi.string().trim().required();
-    const { error, value } = stringSchema.validate(strippedString);
+    const { error, value } = stringSchema.validate(string);
     const errorMessagesArray = this.convertErrors(error);
-    return { error: errorMessagesArray, value };
+    return { error: errorMessagesArray, value: stripHtml(`${value}`).result };
   };
   validateLimit = (limit) => {
-    const strippedLimit = limit ? stripHtml(limit).result : limit;
     const numberSchema = Joi.number().min(1).optional();
-    const { error, value } = numberSchema.validate(strippedLimit);
+    const { error, value } = numberSchema.validate(limit);
     const errorMessagesArray = this.convertErrors(error);
     return { error: errorMessagesArray, value };
   };
   validateMessage = (message) => {
-    const { from, to, text, type } = message;
     const messageSchema = Joi.object({
       from: Joi.string().trim().required(),
       to: Joi.string().trim().required(),
       text: Joi.string().trim().required(),
       type: Joi.string().trim().equal("private_message", "message"),
     });
-    const { error, value } = messageSchema.validate({
-      from: stripHtml(from ?? "").result,
-      to: stripHtml(to ?? "").result,
-      text: stripHtml(text ?? "").result,
-      type: stripHtml(type ?? "").result,
-    });
+    const { error, value } = messageSchema.validate(message);
     const errorMessagesArray = this.convertErrors(error);
-    return { error: errorMessagesArray, value };
+    return {
+      error: errorMessagesArray,
+      value: {
+        from: stripHtml(`${value.from}`).result,
+        to: stripHtml(`${value.to}`).result,
+        text: stripHtml(`${value.text}`).result,
+        type: stripHtml(`${value.type}`).result,
+      },
+    };
   };
 }
 
